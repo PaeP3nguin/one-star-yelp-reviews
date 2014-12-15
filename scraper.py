ff248d779ba942575cfbbb6fd2fbaaa8582adc99
paep3nguin <paep3nguin@gmail.com>
2014-12-14 19:43:14 -0600

    Milestone! Wraps text and overlays

--- scraper.py
+++ scraper.py
@@ -1,43 +1,139 @@
 #!python2
-from lxml import html
 import requests
+import urlparse
 import sys
+import nltk.data
+import urllib
+import urllib2
+import json
+import random
+import textwrap
 
-if len(sys.argv) >= 2:
-    business_name = sys.argv[1]
-else:
-    business_name = 'super-h-mart-niles'
+from lxml import html
+from PIL import Image, ImageFont, ImageDraw
 
-business_page = requests.get('http://www.yelp.com/biz/' + business_name)
-business_tree = html.fromstring(business_page.text)
+def main():
+    # Sets proper Yelp url
+    if len(sys.argv) >= 2:
+        business_url_name = sys.argv[1]
+    else:
+        business_url_name = 'super-h-mart-niles'
 
-review_count = int(business_tree.xpath('//span[@itemprop="reviewCount"]/text()')[0])
-offset = review_count//40 * 40;
+    # Gets html for business
+    business_page = requests.get('http://www.yelp.com/biz/' + business_url_name)
+    business_tree = html.fromstring(business_page.text)
 
-page = requests.get('http://www.yelp.com/biz/' + business_name + '?start=' + str(offset) + '&sort_by=rating_desc')
+    # Gets business name and image
+    business_name = business_tree.xpath('//h1[@itemprop="name"]/text()')[0].strip().replace(' ','%20')
+    img_url = top_google_img_url(business_name)
+    img_name = urlparse.urlparse(img_url).path.split('/')[-1]
+    urllib.urlretrieve(img_url, img_name)
 
-tree = html.fromstring(page.text)
+    # Finds review count on page and retrieves the last page of reviews
+    review_count = int(business_tree.xpath('//*[@id="super-container"]/div/div/div[1]/div[3]/div[1]/div[1]/div[2]/ul[2]/li[1]/span/text()')[0])
+    offset = (review_count - 1) //40 * 40;
+    page = requests.get('http://www.yelp.com/biz/' + business_url_name + '?start=' + str(offset) + '&sort_by=rating_desc')
+    tree = html.fromstring(page.text)
 
-one_star_reviews = tree.xpath('//meta[@itemprop="ratingValue"][@content="1.0"]/../../../../..')
+    # Gets all one star review wrappers
+    one_star_reviews = tree.xpath('//meta[@itemprop="ratingValue"][@content="1.0"]/../../../../..')
 
-highest_rating_sum = -1
-funniest_rating = -1
-review_rating_pairs = []
-for review_wrapper in one_star_reviews:
-    review_text = ' '.join(review_wrapper.xpath('div//p[@itemprop="description"]/text()'))
-    review_ratings = review_wrapper.xpath('*//span[@class="count"]//text()')
-    funny_rating = int(review_wrapper.xpath('*//span[@class="i-wrap ig-wrap-common i-ufc-funny-common-wrap button-content"]/span[@class="count"]//text()')[0])
-    rating_sum = sum(map(int, review_ratings))
-    
-    review_rating_pair = [review_text, rating_sum]
-    review_rating_pairs.append(review_rating_pair)
-    if rating_sum > highest_rating_sum:
-        highest_rated_review = review_rating_pair
-        highest_rating_sum = rating_sum
+    # Makes list of reviews along with their total rating and finds the funniest rated review
+    highest_rating_sum = -1
+    funniest_rating = -1
+    review_rating_pairs = []
+    for review_wrapper in one_star_reviews:
+        review_text = ' '.join(review_wrapper.xpath('div//p[@itemprop="description"]/text()')).replace(u'\xa0', '')
+        review_ratings = review_wrapper.xpath('*//span[@class="count"]//text()')
+        funny_rating = review_wrapper.xpath('*//span[@class="i-wrap ig-wrap-common i-ufc-funny-common-wrap button-content"]/span[@class="count"]//text()')
+        if not funny_rating:
+            funny_rating = 0
+        else:
+            funny_rating = int(funny_rating[0])
+        rating_sum = sum(map(int, review_ratings))
         
-    if funny_rating > funniest_rating:
-        funniest_review = review_rating_pair
-        funniest_rating = funny_rating
+        review_rating_pair = [review_text, rating_sum]
+        review_rating_pairs.append(review_rating_pair)
+        if rating_sum > highest_rating_sum:
+            highest_rated_review = review_rating_pair
+            highest_rating_sum = rating_sum
+            
+        if funny_rating > funniest_rating:
+            funniest_review = review_rating_pair
+            funniest_rating = funny_rating
 
-print highest_rated_review
-print offset
+    # Breaks review text into sentences
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    sentences = tokenizer.tokenize(funniest_review[0])
+    
+    # Either picks sentence randomly or prompts user to select, based on command line arguments
+    if len(sys.argv) >= 3 and sys.argv[2] is 'rand':
+        text = random.choice(sentences)
+    else:
+        for sentence in enumerate(sentences):
+            print sentence
+        print '\n'
+        chosen = int(raw_input("Enter a sentence number: "))
+        text = sentences[chosen]
+
+    draw_text(img_name, text)
+
+def top_google_img_url (biz_name):
+    search_url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s' % biz_name
+    conn = urllib2.urlopen(search_url)
+    try:
+        response = json.loads(conn.read())
+    finally:
+        conn.close()
+    images = response['responseData']['results']
+    for img in images:
+        if big_enough(img):
+            return img['unescapedUrl']
+    return None
+
+def big_enough (img):
+    return int(img['height']) >= 768 and int(img['width']) >= 512
+    
+def draw_text (img_name, text):
+    img = Image.open(img_name)
+    img.thumbnail((1000, 1000), Image.ANTIALIAS)
+    img_width, img_height = img.size
+    draw = ImageDraw.Draw(img, 'RGBA')
+    draw.rectangle([(0, img_height * 0.7), (img_width, img_height * 0.7 + 150)], (0, 0, 0, 150))
+    
+    wrapped = textwrap.wrap(text)
+    font, text_width, text_height = fit_text(wrapped, img_width, 150)
+    
+    text_y_pos = img_height * 0.7 + (150 - text_height) / 2
+    for line in wrapped:
+        line_width, line_height = font.getsize(line)
+        text_x_pos = (img_width - line_width) / 2
+        draw.text((text_x_pos, text_y_pos), line, (255,255,255), font)
+        text_y_pos += line_height + 5
+    
+    img.save('test_' + img_name)
+    
+def fit_text (lines, width, height):
+    test_size = 14
+    test_font = ImageFont.truetype('Bitter-Regular.otf', test_size)
+    text_width, text_height = text_size(test_font, lines)
+    max_width = width * 0.8
+    max_height = height * 0.8
+    while text_width < max_width and text_height < max_height:
+        test_size += 2
+        test_font = ImageFont.truetype('Bitter-Regular.otf', test_size)
+        text_width, text_height = text_size(test_font, lines)
+    return [test_font, text_width, text_height]
+
+def text_size (font, lines):
+    total_width = 0
+    total_height = 0
+    for line in lines:
+        width, height = font.getsize(line)
+        total_height += height + 5
+        if width > total_width:
+            total_width = width
+    return [total_width, total_height]
+
+if __name__ == '__main__':
+    main()
