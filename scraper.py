9688cb43b63f7079e0c34499fbecbd6bee08d8d6
paep3nguin <paep3nguin@gmail.com>
2014-12-15 23:03:33 -0600

    Added lots of comments

--- scraper.py
+++ scraper.py
@@ -85,16 +85,21 @@ def main():
         chosen = ' '.join([sentences[i] for i in indexes])
         text = '"%s" - Yelp, %s, %s/5 stars' % (chosen, business_name, stars)
 
+    # Draws text on the image and starts posting process
     new_name = draw_text(img_name, text)
     post_picture(new_name, text)
 
+# Finds the first Google image search result that meets the size requirements
 def top_google_img_url (biz_name):
+    # Uses Google API to get only photo results
     search_url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&imgtype=photo' % biz_name
     conn = urllib2.urlopen(search_url)
     try:
         response = json.loads(conn.read())
     finally:
         conn.close()
+        
+    # Processes result JSON to find image
     images = response['responseData']['results']
     for img in images:
         if big_enough(img):
@@ -104,16 +109,20 @@ def top_google_img_url (biz_name):
 def big_enough (img):
     return int(img['height']) >= 512 and int(img['width']) >= 512
     
+# Uses PIL to overlay rectangle and text to image
 def draw_text (img_name, text):
+    # Draws semi-transparent black rectangle starting at 70% down the image to increase text visibility
     img = Image.open(img_name)
     img.thumbnail((1000, 1000), Image.ANTIALIAS)
     img_width, img_height = img.size
     draw = ImageDraw.Draw(img, 'RGBA')
     draw.rectangle([(0, img_height * 0.7), (img_width, img_height * 0.7 + 150)], (0, 0, 0, 150))
     
+    # Wraps text and finds largest text size that will fit inside the rectangle
     wrapped = textwrap.wrap(text, 70)
     font, text_width, text_height = fit_text(wrapped, img_width, 150)
     
+    # Draws lines of text on the image
     text_y_pos = img_height * 0.7 + (150 - text_height) / 2
     for line in wrapped:
         line_width, line_height = font.getsize(line)
@@ -121,23 +130,28 @@ def draw_text (img_name, text):
         draw.text((text_x_pos, text_y_pos), line, (255,255,255), font)
         text_y_pos += line_height
     
+    # Saves the image in the captioned/ folder
     new_name = 'capt_' + img_name
     img.save('captioned/' + new_name)
     
     return new_name
     
+# Finds the largest text size possible that fits within the bounds
 def fit_text (lines, width, height):
     test_size = 14
     test_font = ImageFont.truetype('Bitter-Regular.otf', test_size)
     text_width, text_height = text_size(test_font, lines)
     max_width = width * 0.8
     max_height = height * 0.8
+    
+    # Increments font size until it just passes fit specs
     while text_width < max_width and text_height < max_height:
         test_size += 2
         test_font = ImageFont.truetype('Bitter-Regular.otf', test_size)
         text_width, text_height = text_size(test_font, lines)
     return [test_font, text_width, text_height]
 
+# Returns the total size of multiple lines of text in specified font
 def text_size (font, lines):
     total_width = 0
     total_height = 0
@@ -148,6 +162,7 @@ def text_size (font, lines):
             total_width = width
     return [total_width, total_height]
     
+# Posts picture on tumblr
 def post_picture (img_name, caption):
     choice = raw_input("Take a look! Enter p to post, q to queue, d to draft, anything else to cancel:")
     if choice is 'p':
@@ -158,10 +173,13 @@ def post_picture (img_name, caption):
         state = 'draft'
     else:
         return
-        
+    
+    # Put your tumblr API keys here
     client = pytumblr.TumblrRestClient(
       'get yer own'
     )
+    
+    # Fixes caption and posts to tumblr!
     pls = caption.replace(' ','&nbsp;')
     client.create_photo('onestaryelp', state=state, tags=["yelp"], data='captioned/' + str(img_name), caption=pls)
 
